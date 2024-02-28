7efebfba8382349cff81514c7a6d04f6eb278ff3
joshuabutler <butlerja.nhs@gmail.com>
2024-02-27 23:41:30 -0500

    created super user in terminal and removed fake data

--- main_app/views.py
+++ main_app/views.py
@@ -1,20 +1,8 @@
 from django.shortcuts import render
+from .models import Chicken
 
 # Add the following import
 from django.http import HttpResponse
-
-class Chicken: 
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-chickens = [
-  Chicken('Ruth', 'Rhode Island Red', 'Truly a red chicken from Rhode Island.', 700),
-  Chicken('Karen', 'Leghorn', 'Walks around very loudly.', 21),
-  Chicken('Rizzle', 'Frizzle', 'For rizzle this Frizzle is named Rizzle.', 99),
-]
 
 # Define the home view
 def home(request):
