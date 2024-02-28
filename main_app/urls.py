95c163d19ec41d3afa3693fb1cda3327ec00e0c4
joshuabutler <butlerja.nhs@gmail.com>
2024-02-27 22:35:52 -0500

    add chicken class

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
+  path('chickens/', views.chicken_index, name='chicken-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,19 @@ from django.shortcuts import render
 # Add the following import
 from django.http import HttpResponse
 
+class Chicken: 
+  def __init__(self, name, breed, description, age):
+    self.name = name
+    self.breed = breed
+    self.description = description
+    self.age = age
+
+chickens = [
+  Chicken('Ruth', 'Rhode Island Red', 'Truly a red chicken from Rhode Island.', 700),
+  Chicken('Karen', 'Leghorn', 'Walks around very loudly.', 21),
+  Chicken('Rizzle', 'Frizzle', 'For rizzle this Frizzle is named Rizzle.', 99),
+]
+
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
