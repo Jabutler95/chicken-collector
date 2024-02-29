d0d3563b812ca4e7b201deda4cadd8fb3baf0356
joshuabutler <butlerja.nhs@gmail.com>
2024-02-28 20:59:12 -0500

    added feeding into detail view

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner'),
+)
+
 # Create your models here.
 
 class Chicken(models.Model):
@@ -14,4 +20,18 @@ class Chicken(models.Model):
   
   def get_absolute_url(self):
     return reverse("chicken_detail", kwargs={"chicken_id": self.id})
+  
+class Feeding(models.Model):
+  date = models.DateField('Feeding Date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  chicken = models.ForeignKey(Chicken, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
+    
   
