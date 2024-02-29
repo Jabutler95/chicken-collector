462d398313a85b85e4ec5b1339bc43cd9c8580ce
joshuabutler <butlerja.nhs@gmail.com>
2024-02-28 21:45:08 -0500

    complete part 5

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
   path('chickens/<int:pk>/update/', views.ChickenUpdate.as_view(), name='chicken-update'),
   path('chickens/<int:pk>/delete/', views.ChickenDelete.as_view(), name='chicken-delete'),
   path('chickens/<int:chicken_id>/add-feeding/', views.add_feeding, name='add-feeding'),
+  path('toys/create/', views.ToyCreate.as_view(), name='toy-create'),
   path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy-detail'),
   path('toys/', views.ToyList.as_view(), name='toy-index'),
   path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy-update'),
